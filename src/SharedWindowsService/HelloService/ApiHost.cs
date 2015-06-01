33831ac97da5ce9ffdbdecb40bb5efdcd89636ef
Shawn Cicoria <shawn@cicoria.com>
2015-06-01 08:45:37 -0400

    Basic Windows Desktop Stuff
    This is a windows service that can run as a console

---
+++ src/SharedWindowsService/HelloService/ApiHost.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace HelloService
+{
+
+    public class ApiHost
+    {
+        string baseUri = "http://localhost:9000/";
+        public void Run()
+        {
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseUri))
+            {
+                Console.WriteLine("Server running at {0} - press Enter to quit. ", baseUri);
+                Console.ReadLine();
+            }
+        }
+    }
+
+
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
---
+++ src/SharedWindowsService/HelloService/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+
+            var service = new Service1();
+            
+            if (!Environment.UserInteractive)
+            {
+                var servicesToRun = new ServiceBase[] { service };
+                ServiceBase.Run(servicesToRun);
+                return;
+            }
+
+            Console.WriteLine("Running as a Console Application");
+            Console.WriteLine(" 1. Run Service");
+            Console.WriteLine(" 2. Other Option");
+            Console.WriteLine(" 3. Exit");
+            Console.Write("Enter Option: ");
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("Starting Service");
+                    service.Start(args);
+                    Console.WriteLine("Service Start Returned - Press Enter To Exit");
+                    Console.ReadLine();
+                    break;
+
+                case "2":
+                    // TODO!
+                    break;
+            }
+
+            Console.WriteLine("Closing");
+
+        }
+    }
+}
